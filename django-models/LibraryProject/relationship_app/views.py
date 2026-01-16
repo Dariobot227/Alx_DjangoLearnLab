42ef13305f49c871998bd389123935b0fab4d9de
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-16 13:24:59 +0300

    fixing the views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,13 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView
-from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import user_passes_test
-from .models import Book, Library
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import user_passes_test, permission_required
+from .models import Library
 from .models import Book, Author
+
 
 
 # Function-based view: list all books
