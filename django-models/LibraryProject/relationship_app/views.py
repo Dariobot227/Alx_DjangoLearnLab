a2781834becd497561b434c2ebd884c020c8861a
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-16 13:22:14 +0300

    getting closser

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,7 +14,7 @@ from .models import Book, Author
 
 # Function-based view: list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()  
     return render(request, 'relationship_app/list_books.html', {
         'books': books
     })
