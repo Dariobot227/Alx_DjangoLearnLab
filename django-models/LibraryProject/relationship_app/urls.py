f38487394caa16b38eaeb79849dbbc5f85a9e0fe
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-16 12:44:32 +0300

    update to authentication urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import (
     list_books,
     LibraryDetailView,
-    UserLoginView,
-    UserLogoutView,
     register,
     admin_view,
     librarian_view, 
@@ -13,14 +11,17 @@ from .views import (
     edit_book,
     delete_book,
 )
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    # Authentication URLs
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+
+    # Authentication URLs 
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', register, name='register'),
-     path('admin-role/', admin_view, name='admin_view'),
+
+    path('admin-role/', admin_view, name='admin_view'),
     path('librarian-role/', librarian_view, name='librarian_view'),
     path('member-role/', member_view, name='member_view'),
 
