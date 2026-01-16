7111e359e3d27c001d75b8f3da831998c321d38e
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-16 13:00:56 +0300

    final samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -21,9 +21,10 @@ def get_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian
+        return Librarian.objects.get(library=library)
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
+
 
 if __name__ == "__main__":
     print(get_books_by_author("John Doe"))
