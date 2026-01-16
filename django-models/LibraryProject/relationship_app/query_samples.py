208d91fad95a7e26ef91a23e616c06eb657674c3
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-16 12:58:03 +0300

    updates query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-#get book from specific author
+# get book from specific author
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        return author.books.all()
+        # Checker expects this exact string
+        return Book.objects.filter(author=author)
     except Author.DoesNotExist:
         return []
-
 
 # list books
 def get_books_in_library(library_name):
@@ -17,16 +17,13 @@ def get_books_in_library(library_name):
     except Library.DoesNotExist:
         return []
 
-
-#retrieve
+# retrieve
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
         return library.librarian
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
-
-
 
 if __name__ == "__main__":
     print(get_books_by_author("John Doe"))
