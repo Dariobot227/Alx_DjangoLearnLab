e4e535bf57427e8f26b4eb18a93a4cdbe0e79783
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-24 12:58:32 +0300

    admin file changes

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,30 +1,23 @@
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Book
 
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
-
-
 # ----------------------------
 # Custom User Admin
 # ----------------------------
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
     )
 
+admin.site.register(CustomUser, CustomUserAdmin)
+
 # ----------------------------
 # Book Admin
 # ----------------------------
-@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     list_filter = ("author", "publication_year")
     search_fields = ("title", "author__username")
+
+admin.site.register(Book, BookAdmin)
