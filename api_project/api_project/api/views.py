bb9e502a0df5ee9bdea712ac602315498049785a
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-30 17:36:10 +0300

    api project

---
+++ api_project/api_project/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
