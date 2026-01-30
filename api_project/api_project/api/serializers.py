318e34aaa707b5b62b4c115c4800a5effc5dba2c
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-30 17:07:37 +0300

    Revert "git api project"
    This reverts commit f02ecb7692c3d7cbe5a315c12ca9e82d6ce7b037.

--- api_project/api_project/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Book
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
-#Model serialziser auto generates fields based on model defined in models.py
