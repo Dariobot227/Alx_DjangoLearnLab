afc7952dc6548e4f0acd9b8883de405eaa53553c
Darius Kiptoo <dariusngetich025.gmail.com>
2026-01-30 18:01:58 +0300

    finally a worthy opponnent

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
     ],
 }
