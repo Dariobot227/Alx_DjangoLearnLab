cf3a15c21bd8c2decb10c74c01e0a283dc8c3d02
Darius Kiptoo <dariusngetich025.gmail.com>
2026-02-13 18:59:18 +0300

    task 2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,118 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from django.db.models import Q
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post, Comment, Tag
+from .forms import RegisterForm, PostForm, CommentForm
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-created_at"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "blog/confirm_delete.html"
+    success_url = reverse_lazy("post_list")
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("post_list")
+    else:
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email")
+        request.user.save()
+        return redirect("profile")
+    return render(request, "blog/profile.html")
+
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+    return redirect("post_detail", pk=post.id)
+
+
+@login_required
+def edit_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.user != comment.author:
+        return redirect("post_list")
+
+    form = CommentForm(request.POST or None, instance=comment)
+    if form.is_valid():
+        form.save()
+        return redirect("post_detail", pk=comment.post.id)
+
+    return render(request, "blog/post_form.html", {"form": form})
+
+
+@login_required
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.user == comment.author:
+        post_id = comment.post.id
+        comment.delete()
+        return redirect("post_detail", pk=post_id)
+    return redirect("post_list")
+
+
+def posts_by_tag(request, tag_name):
+    tag = get_object_or_404(Tag, name=tag_name)
+    posts = tag.posts.all()
+    return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def search_posts(request):
+    query = request.GET.get("q")
+    posts = Post.objects.filter(
+        Q(title__icontains=query) |
+        Q(content__icontains=query) |
+        Q(tags__name__icontains=query)
+    ).distinct()
+
+    return render(request, "blog/search_results.html", {"posts": posts, "query": query})
