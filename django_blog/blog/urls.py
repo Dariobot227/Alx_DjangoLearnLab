62957b359e019d5149603aafb181533bfd3998da
Darius Kiptoo <dariusngetich025.gmail.com>
2026-02-14 11:31:39 +0300

    day 2 changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
     # Post URLs
     path("", views.PostListView.as_view(), name="post_list"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
